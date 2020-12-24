40821800bb19bf6b93efbca1f04b1bd0dfe93b9e
OElesin <elesin.olalekan@gmail.com>
2020-12-24 11:56:21 +0100

    added query history capability based on localStorage

--- src/data/queryDAO.js
+++ src/data/queryDAO.js
@@ -4,7 +4,7 @@ const queryDao = {
     async addNewQueryToLocalStorage(image){
         console.log("Added images to local storage")
         const result = await localStorage.getItem('userQueryList')
-        const payload = (result !== 'undefined') ? JSON.parse(result) : [];
+        const payload = (result !== 'undefined' || result !== null) ? JSON.parse(result) : [];
         payload.push(image)
         let userContentList = JSON.stringify(payload)
         localStorage.setItem('userQueryList', userContentList)
